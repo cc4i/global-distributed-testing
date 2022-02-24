d52e2592ac866872b2087820204eec8eb0eb77cd
Chuan <chuancc@google.com>
2022-02-25 07:12:16 +0800

    update config

--- hack/provision.sh
+++ hack/provision.sh
@@ -37,7 +37,7 @@ provison_gke() {
     --disk-type "pd-standard" \
     --disk-size "100" \
     --metadata disable-legacy-endpoints=true \
-    --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+    --scopes "https://www.googleapis.com/auth/cloud-platform" \
     --max-pods-per-node "110" \
     --num-nodes "1" \
     --enable-autoscaling --min-nodes "1" --max-nodes "10" \
