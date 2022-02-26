6748c6583999ffc9e2d492ac0b0c5e932dd610f0
Chuan <chuancc@google.com>
2022-02-26 10:25:39 +0800

    Put cmds into script file

---
+++ hack/deploy-worker.sh
@@ -0,0 +1,22 @@
+export PROJECT_ID=${PROJECT_ID}
+
+cp /workspace/kustomization.yaml manifests/worker/kustomization.yaml
+cat manifests/worker/kustomization.yaml
+gcloud version
+regions=`cat ./hack/gke-config`
+master_cluster="testx-`echo ${regions}|awk '{print $1}'`"
+echo "master_cluster => ${master_cluster}"
+
+for loc in ${regions[@]}
+do
+cluster="testx-${loc}"
+if [ ${master_cluster} != ${cluster} ]
+    echo "Deploying into ${cluster} ... ..."
+    gcloud container clusters get-credentials ${cluster} --region ${loc} --project ${PROJECT_ID}
+    kubeclt create ns locust || true
+    kustomize build manifests/worker/. | kubeclt apply -f -
+    echo "Deploying into ${cluster} ... ...done"
+    echo ""
+then
+fi
+done
---
+++ hack/deploy.sh
@@ -0,0 +1,13 @@
+export PROJECT_ID=${PROJECT_ID}
+
+regions=`cat ./gke-config`
+region=`${regions}|awk '{print $1}'`
+master_cluster="testx-${region}"
+
+echo ""
+echo "Retrieve credential from master clutser => ${master_cluster}"
+gcloud container clusters get-credentials ${master_cluster} --region ${region} --project ${PROJECT_ID}
+
+echo "Deploy manifests to master clutser => ${master_cluster}"
+kubeclt create ns locust || true
+kustomize build manifests/master/. | kubeclt apply -f -
