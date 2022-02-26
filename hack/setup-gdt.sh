a8acd6f2537b3b529fabc164b8c76d96f9a22028
Chuan <chuancc@google.com>
2022-02-26 11:31:43 +0800

    update scripts

--- hack/setup-gdt.sh
+++ hack/setup-gdt.sh
@@ -27,6 +27,10 @@ do
     if [ "${status}" == "RUNNING" ]
     then
         echo "GKE Autopilot => ${cluster} is up and runing."
+        gcloud container clusters get-credentials ${cluster} --region ${region} --project ${PROJECT_ID}
+        kubectl config rename-context gke_${PROJECT_ID}_${loc}_${cluster} ${cluster} || true
+        kubectl get node --context ${cluster}
+        cp ~/.kube/config /workspace/.
         echo "..."
 
     else
