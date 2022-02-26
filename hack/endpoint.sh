5608f7d302c30233856ddccabc5621c33b41a9a5
Chuan <chuancc@google.com>
2022-02-26 12:05:22 +0800

    update scripts

--- hack/endpoint.sh
+++ hack/endpoint.sh
@@ -9,7 +9,7 @@ gcloud container clusters get-credentials ${master_cluster} --region ${region} -
 external_ip=""; while [ -z $external_ip ]; do echo "Waiting for end point..."; external_ip=$(kubectl get svc locust-master -n locust --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}" || true); [ -z "$external_ip" ] && sleep 10; done; echo "End point ready-" && echo $external_ip; export endpoint=$external_ip
 echo ${endpoint}
 
-sed -i 's/MASTER_HOST/'${endpoint}'/g' manifests/worker/kustomization.yaml
+sed -i 's/MASTER_HOST/'${endpoint}'/g' ../manifests/worker/kustomization.yaml
 cat ../manifests/worker/kustomization.yaml
 cp ../manifests/worker/kustomization.yaml /workspace
 echo "Access => http://${endpoint}/"
