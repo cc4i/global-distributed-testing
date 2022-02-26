fbdbdffbb92687e8b73359d7d10d06d79d66642c
Chuan <chuancc@google.com>
2022-02-26 13:21:39 +0800

    update scripts

--- hack/deploy-worker.sh
+++ hack/deploy-worker.sh
@@ -1,4 +1,18 @@
+echo "Inside => ${PROJECT_ID}"
 export PROJECT_ID=${PROJECT_ID}
+if [ -z "${PROJECT_ID}" ]
+then
+    export PROJECT_ID=`gcloud config get-value project`
+    if [ -z "${PROJECT_ID}" ]
+    then
+        echo "Pls setup project id to run this script."
+        exit 1
+    fi
+    echo "Using defautl project id => ${PROJECT_ID} instead."
+
+fi  
+export PROJECT_NUM=`gcloud projects list --filter PROJECT_ID=${PROJECT_ID} --format "value(PROJECT_NUMBER)"`
+env
 
 cp /workspace/kustomization.yaml ../manifests/worker/kustomization.yaml
 cat ../manifests/worker/kustomization.yaml
