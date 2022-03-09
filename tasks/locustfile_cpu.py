08e68fcae2b3540e6f8a4e326f62bfdaa2f21463
Chuan <chuancc@google.com>
2022-03-09 14:04:56 +0800

    Using SA to get identity token

--- tasks/locustfile_cpu.py
+++ tasks/locustfile_cpu.py
@@ -1,22 +1,27 @@
 import time
 import random
-from locust import FastHttpUser, task, between
+from locust import HttpUser, task, between
+
 import google.auth
 import google.auth.transport.requests
+import google.oauth2.id_token
 
-creds, project = google.auth.default()
 
-class EmulatedUser(FastHttpUser):
+
+class EmulatedUser(HttpUser):
     wait_time = between(0.5, 5)
-    
-    # creds.valid is False, and creds.token is None
-    # Need to refresh credentials to populate those
-    auth_req = google.auth.transport.requests.Request()
-    creds.refresh(auth_req)
-    # Now you can use creds.token
-    def __init__(self, environment):
-        print(creds.token)
+
+
+    def on_start(self):
+        self.target_audience = self.client.base_url
+        self.url = self.client.base_url
+        self.auth_req = google.auth.transport.requests.Request()
+        self.id_token = google.oauth2.id_token.fetch_id_token(self.auth_req, self.target_audience)
+        print(self.id_token)
+
 
     @task
-    def test_view_index(self):
-        self.client.get(path="/bite/10", headers={"Authorization": "Bearer {}".format(creds.token)})
+    def test_bite_cpu(self):
+        bearer = "Bearer {}".format(self.id_token)
+        respone = self.client.get(url="/bite/10", headers={"Authorization": bearer})
+        print(respone)
