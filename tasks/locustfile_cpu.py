540a41dd1d850e1c145a8e4a8f6fe2686005689c
Chuan <chuancc@google.com>
2022-03-18 21:48:23 +0800

    update logging

--- tasks/locustfile_cpu.py
+++ tasks/locustfile_cpu.py
@@ -1,12 +1,12 @@
-import time
-import random
 from locust import HttpUser, task, between
 
 import google.auth
 import google.auth.transport.requests
 import google.oauth2.id_token
+import logging
+import sys
 
-
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 class EmulatedUser(HttpUser):
     wait_time = between(0.5, 5)
@@ -15,9 +15,12 @@ class EmulatedUser(HttpUser):
     def on_start(self):
         self.target_audience = self.client.base_url
         self.url = self.client.base_url
-        self.auth_req = google.auth.transport.requests.Request()
-        self.id_token = google.oauth2.id_token.fetch_id_token(self.auth_req, self.target_audience)
-        print(self.id_token)
+        try:
+            self.auth_req = google.auth.transport.requests.Request()
+            self.id_token = google.oauth2.id_token.fetch_id_token(self.auth_req, self.target_audience)
+        except Exception as e:
+            logging.error(e)
+        logging.info(self.id_token)
 
 
     # @task
@@ -29,8 +32,12 @@ class EmulatedUser(HttpUser):
     @task(80)
     def test_write2bt(self):
         bearer = "Bearer {}".format(self.id_token)
-        print("bearer -> {}".format(bearer))
-        respone = self.client.post(url="/write2bt", headers={"Authorization": bearer}, data={"test": "data"})
-        print("request header -> {}".format(self.client.headers))
-        print("respone header -> {}".format(respone.headers))
-        print("response body -> {}".format(respone.content))
+        logging.info("bearer -> {}".format(bearer))
+        try:
+            respone = self.client.post(url="/write2bt", headers={"Authorization": bearer}, data={"test": "data"})
+        except Exception as e:
+            logging.error(e)
+
+        logging.info("request header -> {}".format(self.client.headers))
+        logging.info("respone header -> {}".format(respone.headers))
+        logging.info("response body -> {}".format(respone.content))
